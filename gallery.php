0ffb6cb6630419cf53bbd1522329dcbb815413ee
David <david.1990@ymail.com>
2014-12-20 13:25:51 +0000

    gallery, second take

---
+++ gallery.php
@@ -0,0 +1,46 @@
+<div class="fluid_container">
+    <div class="camera_wrap" id="camera_wrap_1">
+        <div data-thumb="camera/images/slides/thumbs/bridge.jpg" data-src="camera/images/slides/bridge.jpg">
+            <div class="camera_caption fadeFromBottom">
+                Tekst o kompaniji 
+                <br>
+                Jos teksta
+                <br>
+                I jos nekog finog teksta
+                <br>
+                I jos malo finog teksta
+            </div>
+        </div>
+        <div data-thumb="camera/images/slides/thumbs/leaf.jpg" data-src="camera/images/slides/leaf.jpg">
+            <div class="camera_caption fadeIn">
+                It uses a light version of jQuery mobile, <em>navigate the slides by swiping with your fingers</em>
+            </div>
+        </div>
+        <div data-thumb="camera/images/slides/thumbs/road.jpg" data-src="camera/images/slides/road.jpg">
+            <div class="camera_caption moveFromLeft">
+                <em>It's completely free</em> (even if a donation is appreciated)
+            </div>
+        </div>
+        <div data-thumb="camera/images/slides/thumbs/sea.jpg" data-src="camera/images/slides/sea.jpg">
+            <div class="camera_caption fadeFromBottom">
+                Camera slideshow provides many options <em>to customize your project</em> as more as possible
+            </div>
+        </div>
+        <div data-thumb="camera/images/slides/thumbs/shelter.jpg" data-src="camera/images/slides/shelter.jpg">
+            <div class="camera_caption fadeFromBottom">
+                It supports captions, HTML elements and videos and <em>it's validated in HTML5</em> (<a href="http://validator.w3.org/check?uri=http%3A%2F%2Fwww.pixedelic.com%2Fplugins%2Fcamera%2F&amp;charset=%28detect+automatically%29&amp;doctype=Inline&amp;group=0&amp;user-agent=W3C_Validator%2F1.2" target="_blank">have a look</a>)
+            </div>
+        </div>
+        <div data-thumb="camera/images/slides/thumbs/tree.jpg" data-src="camera/images/slides/tree.jpg">
+            <div class="camera_caption fadeFromBottom">
+                Different color skins and layouts available, <em>fullscreen ready too</em>
+            </div>
+        </div>
+        <div data-thumb="camera/images/slides/thumbs/vimeo.jpg" data-video="hide"  data-src="camera/images/slides/vimeo.jpg">
+            <iframe src="http://player.vimeo.com/video/1084537" width="100%" height="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+        </div>
+    </div><!-- #camera_wrap_1 -->
+
+</div><!-- .fluid_container -->
+
+<div style="clear:both; display:block; height:100px"></div>
