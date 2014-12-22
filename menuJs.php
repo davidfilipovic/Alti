55d7a5f64b13df29cfcf5d627aece99867886751
markotodorovic <marko.todorovic@alti.rs>
2014-12-22 11:11:19 +0100

    hover

--- menuJs.php
+++ menuJs.php
@@ -128,9 +128,9 @@
         </script>
     </div>
 	<div class='social'>
-                <a><img src='icon/facebook.png' width='33px' height='33px'></a>
-                <a><img src='icon/twitter.png' width='33px' height='33px'></a><br>
-                <a><img src='icon/google.png' width='33px' height='33px'></a>
-                <a><img src='icon/linkedin.png' width='33px' height='33px'></a>
+                <a><img src='icon/facebook.png' onmouseover="this.src='icon/facebook_h.png'" onmouseout="this.src='icon/facebook.png'" width='33px' height='33px'></a>
+                <a><img src='icon/twitter.png' onmouseover="this.src='icon/twitter_h.png'" onmouseout="this.src='icon/twitter.png'" width='33px' height='33px'></a><br>
+                <a><img src='icon/google.png' onmouseover="this.src='icon/google_h.png'" onmouseout="this.src='icon/google.png'" width='33px' height='33px'></a>
+                <a><img src='icon/linkedin.png' onmouseover="this.src='icon/linkedin_h.png'" onmouseout="this.src='icon/linkedin.png'" width='33px' height='33px'></a>
             </div>
 </div>
